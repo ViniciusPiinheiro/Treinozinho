c36569990e813775ff610367c974c6c3c1dcb1c4
Vinícius Pinheiro <viniciussp.on@gmail.com>
2021-07-21 10:40:00 -0400

    Initial Commit

---
+++ Treinozinho/Controllers/RelatedMoviesViewController.swift
@@ -0,0 +1,15 @@
+//
+//  RelatedMoviesViewController.swift
+//  Treinozinho
+//
+//  Created by Vinícius Pinheiro on 21/07/21.
+//
+
+import UIKit
+
+class RelatedMoviesViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()        
+    }
+}
